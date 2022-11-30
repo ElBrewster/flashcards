8c1ad79305b90b9612e5f663b5b7d584f91b4893
El Brewster <brewsterel3@gmail.com>
2022-11-29 18:10:20 -0800

    Add small fix to Card test file and some test functionality to Round class

--- src/Round.js
+++ src/Round.js
@@ -1,3 +1,11 @@
+class Round {
+    constructor(card1) {
+        //correct/incorrect guesses
+        //current card(index position 0)
+
+    }
+}
 //takes in responses
 //records guesses
+
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,5 +1,44 @@
 const chai = require('chai');
 const expect = chai.expect;
-const Turn = require('../src/Turn');
+const Round = require('../src/Card');
 const Deck = require('../src/Deck');
-const Card = require ('../src/Card');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+
+describe('Round', function() {
+    let deck;
+    let card1;
+    let card2;
+    let card3;
+    let cardArray;
+    let round;
+    // let turn;
+    beforeEach(function() {
+        console.log('Starting Round...');
+        cardArray = [card1, card2, card3];
+        card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+        card2 = new Card(2, "What is a comma-separated list of related values?",  ["array", "object", "function"], "array");
+        card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+        deck = new Deck(cardArray);
+        round = new Round();
+        // turn = new Turn();
+    });
+
+    it('should be a function', () => {
+        expect(Round).to.be.a('function');
+    });
+
+
+    it('should instantiate our good friend Turn', () => {
+        expect(round).to.be.an.instanceof(Round);
+    });
+
+    it('should have a property to identify the current card', () => {
+
+    })
+    //returnCurrentCard(); returns current card being played
+    //takeTurn(); updates 'turns' count, evaluates guesses, gives feedback, stores ids of incorrect guesses
+        //instantiates a new turn to access...?(yes)
+    //calculatePercentCorrect(); calculates and returns the percentage of correct guesses
+    //endRound(); prints the following: '**Round over!**You answerd<>% of the questions correctly!'
+})
